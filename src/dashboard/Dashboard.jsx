38bb7f2c122762ef059e734b1272975c0c86dd52
grantfogle <grantjfogle@gmail.com>
2021-11-02 20:41:14 -0600

    adding river table

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Search from './search/Search';
 import SearchForm from './search/SearchForm';
+import RiverTable from './RiverTable/RiverTable';
 
 const Dashboard = () => {
     return (
         <>
-            <h1>Dashboard works</h1>
+            <h2>Search rivers</h2>
             {/* <Search /> */}
             <SearchForm />
+            <RiverTable />
         </>
         // search bar
         // view of current favorite rivers
