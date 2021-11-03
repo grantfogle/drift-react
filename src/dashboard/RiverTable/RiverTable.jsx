f7a93f3db59d9d72d728bbb11e86d25fddc095ed
grantfogle <grantjfogle@gmail.com>
2021-11-02 21:55:25 -0600

    river table added

--- src/dashboard/RiverTable/RiverTable.jsx
+++ src/dashboard/RiverTable/RiverTable.jsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 const RiverTable = () => {
 
@@ -14,45 +15,49 @@ const RiverTable = () => {
     }
 
     const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('Upper Colorado River', 159, 4.0, 'Colorado River', 'CO'),
+        createData('Ogden River', 237, 3.0, 'Green River', 'UT'),
+        createData('Yellowstone River (Bozeman)', 262, 2.5, 'Madison River', 'MT'),
+        createData('Eagle River (Red Cliff)', 305, 3.7, 'Colorado River', 'CO'),
+        createData('Green River', 356, 4.2, 'Green River', 'UT'),
     ];
     return (
         <>
             <div className="river-table-nav"></div>
             <h3>River table works</h3>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+            <Box sx={{
+
+            }}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ maxWidth: 900, margin: 'auto' }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>River Section</TableCell>
+                                <TableCell align="right">Flow (cfs)</TableCell>
+                                <TableCell align="right">Rating</TableCell>
+                                <TableCell align="right">Watershed</TableCell>
+                                <TableCell align="right">State</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </>
     );
 }
