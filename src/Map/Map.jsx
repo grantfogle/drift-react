fc09095b3ec284f2557d23f3af60e069130e358f
grantfogle <grantjfogle@gmail.com>
2021-12-17 21:28:01 -0700

    adding use map events hook

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './style.css';
-import 'leaflet/dist/leaflet.css';
-import RiverInfo from './riverInfo/RiverInfo';
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents
+} from "react-leaflet";
+import "./style.css";
+import "leaflet/dist/leaflet.css";
+import RiverInfo from "./riverInfo/RiverInfo";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+
+const MyMap = () => {
+  const map = useMapEvents({
+    click: () => {
+      map.locate();
+    },
+    locationfound: location => {
+      console.log("location found:", location);
+    }
+  });
+  return null;
+};
 
 const Map = () => {
-    return (
-        <div className='map-container'>
-            <MapContainer center={[39.3689, -107.0328]} zoom={13} scrollWheelZoom={false} className='map'>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-                <RiverInfo />
-            </MapContainer>
-        </div>
-    );
+  const position = [39.3689, -107.0328];
+
+  return (
+    <div className="map-container">
+      <MapContainer
+        center={position}
+        zoom={10}
+        scrollWheelZoom={false}
+        className="map"
+      >
+        <MyMap />
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <RiverInfo />
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
