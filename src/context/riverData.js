e64638e7581af0f4ffd9b9373994b24d21488e98
grantfogle <grantjfogle@gmail.com>
2022-01-07 20:09:38 -0700

    adding additional river data to component

--- src/context/riverData.js
+++ src/context/riverData.js
@@ -1,114 +1,124 @@
 export const riverData = [
-    {
-        id: 1,
-        state: 'CO',
-        river: 'Colorado River',
-        watershed: 'Colorado River',
-        geoTag: 'Kremmling',
-        currentFlowCFS: 1450,
-        usgsId: '2355',
-        alerts: ['offColor', 'highWater']
-    },
-    {
-        id: 2,
-        state: 'CO',
-        river: 'Roaring Fork',
-        watershed: 'Colorado River',
-        geoTag: 'Aspen',
-        currentFlowCFS: 300,
-        usgsId: '23552',
-        alerts: ['lowFlows', 'warmWater']
-    },
-    {
-        id: 3,
-        state: 'CO',
-        river: 'Eagle River',
-        watershed: 'Colorado River',
-        geoTag: 'Eagle',
-        currentFlowCFS: 600,
-        usgsId: '235521',
-        alerts: []
-    },
-    {
-        id: 4,
-        state: 'CO',
-        river: 'Frying Pan',
-        watershed: 'Colorado River',
-        geoTag: 'Reudi Reservoir',
-        currentFlowCFS: 120,
-        usgsId: '235522',
-        alerts: []
-    },
-    {
-        id: 5,
-        state: 'CO',
-        river: 'Gore Creek',
-        watershed: 'Colorado River',
-        geoTag: 'Vail',
-        currentFlowCFS: 200,
-        usgsId: '235527',
-        alerts: ['warmWater']
-    },
-    {
-        id: 6,
-        state: 'CO',
-        river: 'South Platte',
-        watershed: 'South Platte',
-        geoTag: 'Deckers',
-        currentFlowCFS: 320,
-        usgsId: '235529',
-        alerts: ['lowFlows', 'warmWater']
-    },
-    {
-        id: 7,
-        state: 'CO',
-        river: 'Eagle River',
-        watershed: 'Colorado River',
-        geoTag: 'Dotsero',
-        currentFlowCFS: 420,
-        usgsId: '235520',
-        alerts: ['frozen']
-    },
-    {
-        id: 8,
-        state: 'CO',
-        river: 'Colorado River',
-        watershed: 'Colorado River',
-        geoTag: 'Gore Creek',
-        currentFlowCFS: 2000,
-        usgsId: '2355255',
-        alerts: ['highWater']
-    },
-    {
-        id: 9,
-        state: 'CO',
-        river: 'Crystal River',
-        watershed: 'Colorado River',
-        geoTag: 'Carbondale',
-        currentFlowCFS: 155,
-        usgsId: '2355277',
-        alerts: []
-    },
-    {
-        id: 10,
-        state: 'CO',
-        river: 'Piney River',
-        watershed: 'Colorado River',
-        geoTag: 'Vail',
-        currentFlowCFS: 120,
-        usgsId: '2355269',
-        alerts: []
-    }
+  {
+    id: 1,
+    state: "CO",
+    river: "Colorado River",
+    watershed: "Colorado River",
+    geoTag: "Kremmling",
+    currentFlowCFS: 1450,
+    usgsId: "2355",
+    alerts: ["offColor", "highWater"]
+  },
+  {
+    id: 2,
+    state: "CO",
+    river: "Roaring Fork",
+    watershed: "Colorado River",
+    geoTag: "Aspen",
+    currentFlowCFS: 300,
+    usgsId: "23552",
+    alerts: ["lowFlows", "warmWater"]
+  },
+  {
+    id: 3,
+    state: "CO",
+    river: "Eagle River",
+    watershed: "Colorado River",
+    geoTag: "Eagle",
+    currentFlowCFS: 600,
+    usgsId: "235521",
+    alerts: []
+  },
+  {
+    id: 4,
+    state: "CO",
+    river: "Frying Pan",
+    watershed: "Colorado River",
+    geoTag: "Reudi Reservoir",
+    currentFlowCFS: 120,
+    usgsId: "235522",
+    alerts: []
+  },
+  {
+    id: 5,
+    state: "CO",
+    river: "Gore Creek",
+    watershed: "Colorado River",
+    geoTag: "Vail",
+    currentFlowCFS: 200,
+    usgsId: "235527",
+    alerts: ["warmWater"]
+  },
+  {
+    id: 6,
+    state: "CO",
+    river: "South Platte",
+    watershed: "South Platte",
+    geoTag: "Deckers",
+    currentFlowCFS: 320,
+    usgsId: "235529",
+    alerts: ["lowFlows", "warmWater"]
+  },
+  {
+    id: 7,
+    state: "CO",
+    river: "Eagle River",
+    watershed: "Colorado River",
+    geoTag: "Dotsero",
+    currentFlowCFS: 420,
+    usgsId: "235520",
+    alerts: ["frozen"]
+  },
+  {
+    id: 8,
+    state: "CO",
+    river: "Colorado River",
+    watershed: "Colorado River",
+    geoTag: "Gore Creek",
+    currentFlowCFS: 2000,
+    usgsId: "2355255",
+    alerts: ["highWater"]
+  },
+  {
+    id: 9,
+    state: "CO",
+    river: "Crystal River",
+    watershed: "Colorado River",
+    geoTag: "Carbondale",
+    currentFlowCFS: 155,
+    usgsId: "2355277",
+    alerts: []
+  },
+  {
+    id: 10,
+    state: "CO",
+    river: "Piney River",
+    watershed: "Colorado River",
+    geoTag: "Vail",
+    currentFlowCFS: 120,
+    usgsId: "2355269",
+    alerts: []
+  },
+  {
+    id: 10,
+    state: "CO",
+    river: "Clear Creek",
+    watershed: "South Platte",
+    geoTag: "Golden",
+    currentFlowCFS: 90,
+    usgsId: "2355269",
+    alerts: []
+  }
 ];
 
 export const watershedData = [
-    'Colorado River',
-    'South Platte',
-    'Arkansas',
-    'Rio Grande',
-    'San Juan',
-    'Gunnison',
-    'North Platte'
+  "Colorado River",
+  "South Platte",
+  "Arkansas",
+  "Rio Grande",
+  "San Juan",
+  "Gunnison",
+  "North Platte"
 ];
 
 export const userFavorited = [1, 2, 4, 6];
