3f65e38df7f80c01335114092ac7a77f5b383312
grantfogle <grantjfogle@gmail.com>
2022-03-27 21:08:24 -0600

    removing map component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Main from "./components/main/Main";
 import "./App.css";
-// import Map from "./components/map/Map";
 import MapBoxMap from "./components/map/mapbox/MapBox";
 import { ProvideAuth } from "./use-auth.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -13,7 +12,6 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<Main />}></Route>
-            {/* <Route path="/map" element={<Map />}></Route> */}
             <Route path="/map" element={<MapBoxMap />}></Route>
           </Routes>
         </div>
